1f5bf3d47222ae67a158c7f4f1d951393abd6284
Oyinoye <oyinlolu@gmail.com>
2021-09-02 00:17:29 +0100

    Edited app to be served from flask

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import flask
 import dash
 import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
+# app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+# server = app.server
+
+server = flask.Flask(__name__)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], server=server)
