77242989586cee880521ac13f6bf6231fc807068
Oyinoye <oyinlolu@gmail.com>
2021-09-01 18:57:57 +0100

    resolve inputs dash

--- apps/app3.py
+++ apps/app3.py
@@ -30,7 +30,8 @@ layout = html.Div([
 
 @app.callback(
     Output('app-1-display-value', 'children'),
-    Input('app-1-dropdown', 'value'))
+    [Input('app-1-dropdown', 'value')]
+    )
 def display_value(value):
     return 'You have selected "{}"'.format(value)
 
