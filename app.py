7a351c11fb44cae67137a833f485002801ccdc67
Oyinoye <oyinlolu@gmail.com>
2021-09-02 14:09:32 +0100

    converting back to dash instead of embedded dash in flask.

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import dash
 import dash_bootstrap_components as dbc
 import os
 
-# app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
-# server = app.server
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
-server = flask.Flask(__name__)
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], server=server)
+# server = flask.Flask(__name__)
+# app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], server=server)
 server.secret_key = os.environ.get('secret_key', 'secret')
